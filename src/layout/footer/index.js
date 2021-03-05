fef500b861d71405e8617c170e5794fce33e85f8
thaind(Alden) <dinovative@Dinovatives-Mac-mini.local>
2021-03-05 18:31:36 +0700

    transform to function component footer, change head title

--- src/layout/footer/index.js
+++ src/layout/footer/index.js
@@ -4,29 +4,27 @@
  * User: Danh Le / danh.le@dinovative.com
  * Date: 2020-04-12 23:17:05
  */
-import React, { Component } from 'react'
+import React from 'react'
 
-class Footer extends Component {
-  render () {
-    return (
-      <footer id="footer" className="footer">
-        <div className="footer-left">
-          <div className="address">11 D1 Street, Tan Hung Ward, District 7, Ho Chi Minh City</div>
-          <div className="contact">(+84) 28 73007880  |  contact@dinosys.vn</div>
-        </div>
+const Footer = () => {
+  return (
+    <footer id="footer" className="footer">
+      <div className="footer-left">
+        <div className="address">11 D1 Street, Tan Hung Ward, District 7, Ho Chi Minh City</div>
+        <div className="contact">(+84) 28 73007880  |  contact@dinosys.vn</div>
+      </div>
 
-        <div className="footer-right">
-          <div className="social">
-            <a href="#">Linkedin</a>
-            <a href="#">Facebook</a>
-          </div>
-          <div className="policy">
-            © 2015 – 2021 Dinosys |  Privacy Policy
-          </div>
+      <div className="footer-right">
+        <div className="social">
+          <a href="#">Linkedin</a>
+          <a href="#">Facebook</a>
         </div>
-      </footer>
-    )
-  }
+        <div className="policy">
+          © 2015 – 2021 Dinosys |  Privacy Policy
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
