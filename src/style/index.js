f82b6ff336be7ef6c7db279d3e615a5efd83a3cd
ngan.tran@dinovative.com <myngan@myngan.local>
2021-03-08 16:44:28 +0700

    add font Roboto

--- src/style/index.js
+++ src/style/index.js
@@ -1,8 +1,8 @@
 // load bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-// autoload all style
-import './_autoload.scss'
-
 // import font face
 import '_static/fonts/style.css'
+
+// autoload all style
+import './_autoload.scss'
