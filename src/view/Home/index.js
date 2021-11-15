2ac999b6820d5f72ffba832b6c554126510a5cef
MP <minhphi.nguyen@dinovative.com>
2021-11-15 12:39:59 +0700

    edit text for home page

--- src/view/Home/index.js
+++ src/view/Home/index.js
@@ -12,8 +12,13 @@ const index = () => {
     <div className="home">
       <img src={Logo} alt="logo"/>
       <div className="home-content">
-        <div className="heading-5 home-content__header">Embark the future with us.</div>
-        <div className="heading-1 home-content__title">Technology experts and strong blockchain believers.</div>
+        <div className="heading-5 home-content__header">Embark the future with us...</div>
+        <div className="heading-2 home-content__title">
+          <div>Technology experts and strong blockchain believers.
+            Our passion is making a difference with
+          </div>
+          <div>great products.</div>
+        </div>
         <div className="home-content__link">
           <div className="heading-6 home-content__link--text">GET TO KNOW US</div>
           <img src={ArrowRight} alt="arrow-go"/>
