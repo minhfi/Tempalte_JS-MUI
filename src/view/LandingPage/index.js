ec00744b6532264b04b40506c14c810301b2928c
MP <minhphi.nguyen@dinovative.com>
2021-12-21 12:01:28 +0700

    update animation

--- src/view/LandingPage/index.js
+++ src/view/LandingPage/index.js
@@ -94,7 +94,7 @@ const index = () => {
         <SwitchTransition>
           <CSSTransition
             key={active}
-            classNames="main-fade"
+            classNames="main-fade-up"
             timeout={{ enter: 750, exit: 200 }}
           >
             {LAYOUT}
